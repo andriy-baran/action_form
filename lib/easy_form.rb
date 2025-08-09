6dc8ef5f6f8e12216f6a6de1a3b48f79831bb0ce
Andrii Baran <andrii.baran@newfireglobal.com>
2025-08-09 18:17:39 +0300

    bundle gem init

---
+++ lib/easy_form.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "easy_form/version"
+
+module EasyForm
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/easy_form_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe EasyForm do
+  it "has a version number" do
+    expect(EasyForm::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
